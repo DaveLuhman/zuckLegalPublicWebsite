ec7a387264227229b68379ec1c530bbcc97c1bab
Dave Luhman <daveluhman@gmail.com>
2023-01-16 07:03:38 -0600

    added caprover config and dockerfile

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+dotenv.config({ path: './config/config.env' }); //dotenv file
+const PORT = process.env.PORT || 3000
+const app = express();
+
+// Logging
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+};
+app.use('/', express.static('public'));
+app.listen(PORT), console.log(`Server running on ${process.env.NODE_ENV} on port ${PORT}`);
